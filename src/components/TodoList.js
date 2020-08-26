ea52fc061a616d8a00b32049c379d64ff8d84602
liukenan <liukenan@meetstarinc.com>
2020-08-26 18:31:46 +0800

    redux学习

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
-import{Input,Button} from 'antd'
+// import{Input,Button} from 'antd'
 const data=[
-    
+    '早上八点开晨会',
+    '中午十二点吃饭',
+    '晚上七点半回家'
 ]
 class TodoList extends Component {
     render() {
+        let list=data.map((number)=><li key={number}>{number}</li>);
         return (
             <div style={{margin:'10px'}}>
                <div>
-                   <Input placeholder="write"  style={{width:'250px'}}/>
-                   <Button type="primary" style={{marginLeft:'10px'}}>增加</Button>
+                   <input placeholder="write"  style={{width:'250px'}}/>
+                   <button type="primary" style={{marginLeft:'10px'}}>增加</button>
+               </div>
+               <div style={{margin:'10px'}}>
+                <ul>
+                    {list}
+                </ul>
+
+
                </div>
             </div>
         );
