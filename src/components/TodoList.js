08acd6365fad362e09ecd4edb3774b5ace54ed24
liukenan <liukenan@meetstarinc.com>
2020-08-26 22:14:54 +0800

    redux 发布订阅,更新dom

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -11,13 +11,31 @@ class TodoList extends Component {
      constructor(props){
             super(props)
             this.state=store.getState()
+            this.changeInputValue=this.changeInputValue.bind(this);
+            this.storeChange=this.storeChange.bind(this);
+            // console.log(this.state);
         }
+    storeChange(){
+        this.setState(store.getState());
+    }
+    
+    changeInputValue(e){
+        const action={
+            type:'changeInput',
+            value:e.target.value
+        }
+        store.dispatch(action);
+    }
     render() {
         let list=this.state.list.map((number)=><li key={number}>{number}</li>);
+        store.subscribe(this.storeChange);
         return (
             <div style={{margin:'10px'}}>
                <div>
-                   <input placeholder={this.state.inputValue}  style={{width:'250px'}}/>
+                   <input placeholder={this.state.inputValue}  style={{width:'250px'}}
+                       onChange={this.changeInputValue}
+                       value={this.state.inputValue}
+                   />
                    <button type="primary" style={{marginLeft:'10px'}}>增加</button>
                </div>
                <div style={{margin:'10px'}}>
